77f603f5b90f142cf3bbcaccdc28147fcf365677
Nobrain11 <chainpulseink@gmail.com>
2026-08-14 10:35:58 +0100

    Add constants for blockchain configuration

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || '4663');
+export const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL || 'https://rpc.robinhoodchain.com';
+export const EXPLORER = process.env.NEXT_PUBLIC_EXPLORER || 'https://explorer.robinhoodchain.com';
+export const DEX_SCREENER_API = 'https://api.dexscreener.com/latest/dex/search';
